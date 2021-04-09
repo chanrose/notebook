7f4e6aaed6bbffc499eaf35811a8dfbb311c6b50
Chanbroset <bdchanbroset@gmail.com>
2021-04-09 07:34:35 +0700

    implement auth and many to one

---
+++ README.md
@@ -0,0 +1,8 @@
+# Note Keeper 
+Latest update on April 09 by Broset
+
+## Updated:
+- Add authentication
+- Implement One to Many relationship
+- Modify the view to make sure the functionality is constrain to the current user
+
--- note_keeper/models.py
+++ note_keeper/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     title = models.CharField(max_length=200, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     modify_date = models.DateTimeField('date published', auto_now_add=True)
     doc = models.DateTimeField('date published', auto_now_add=True)
